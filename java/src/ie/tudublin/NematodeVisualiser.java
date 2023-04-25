066ac624da54bca0828d7e08a507cc93cc7a6fb4
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-04-26 00:38:43 +0100

    Implemented nematodes.csv to nematodes array list

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -1,6 +1,7 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import processing.core.PApplet;
 import processing.data.Table;
@@ -8,12 +9,23 @@ import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	ArrayList<Nematode> nematodes;
 
 	public void keyPressed()
-	{		
-		if (keyCode == LEFT)
-		{
-		}		
+	{
+		switch (key) {
+			case LEFT: {
+
+				break;
+			}
+			case RIGHT: {
+
+				break;
+			}
+			default: {
+				break;
+			}
+		}
 	}
 
 
@@ -22,20 +34,29 @@ public class NematodeVisualiser extends PApplet
 		size(800, 800);
 	}
 
-	public void setup() 
+	public void setup()
 	{
 		colorMode(HSB);
 		background(0);
-		smooth();				
+		smooth();
+
+		// Nematodes
+		nematodes = new ArrayList<Nematode>();
+		loadNematodes();
+		print("Nematodes loaded");
 	}
-	
+
 
 	public void loadNematodes()
 	{
+		Table table = loadTable("nematodes.csv", "header");
+		for (TableRow tr : table.rows()) {
+			nematodes.add(new Nematode(tr));
+		}
 	}
 
 
 	public void draw()
-	{	
+	{
 	}
 }
